21c7f7008b97ec8794c16fcf4eef8d82969dd6f0
meral-uraz <urazmeral@gamil.com>
2024-01-29 21:36:44 +0300

    Proje dosyası ekle.

---
+++ Form1.Designer.cs
@@ -0,0 +1,276 @@
+﻿namespace Timer
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.txtState = new System.Windows.Forms.Button();
+            this.txtReset = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.lbHour = new System.Windows.Forms.Label();
+            this.lbMinute = new System.Windows.Forms.Label();
+            this.lbSecond = new System.Windows.Forms.Label();
+            this.lbSplit = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.btnRes = new System.Windows.Forms.Button();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.lsTimes = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // txtState
+            // 
+            this.txtState.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtState.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.txtState.ForeColor = System.Drawing.Color.RoyalBlue;
+            this.txtState.Location = new System.Drawing.Point(27, 18);
+            this.txtState.Name = "txtState";
+            this.txtState.Size = new System.Drawing.Size(111, 55);
+            this.txtState.TabIndex = 0;
+            this.txtState.Text = "START";
+            this.txtState.UseVisualStyleBackColor = true;
+            this.txtState.Click += new System.EventHandler(this.txtState_Click);
+            // 
+            // txtReset
+            // 
+            this.txtReset.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtReset.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.txtReset.ForeColor = System.Drawing.Color.RoyalBlue;
+            this.txtReset.Location = new System.Drawing.Point(144, 18);
+            this.txtReset.Name = "txtReset";
+            this.txtReset.Size = new System.Drawing.Size(111, 55);
+            this.txtReset.TabIndex = 1;
+            this.txtReset.Text = "RESET";
+            this.txtReset.UseVisualStyleBackColor = true;
+            this.txtReset.Click += new System.EventHandler(this.txtReset_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // lbHour
+            // 
+            this.lbHour.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lbHour.AutoSize = true;
+            this.lbHour.ForeColor = System.Drawing.Color.RoyalBlue;
+            this.lbHour.Location = new System.Drawing.Point(51, 122);
+            this.lbHour.Name = "lbHour";
+            this.lbHour.Size = new System.Drawing.Size(14, 16);
+            this.lbHour.TabIndex = 3;
+            this.lbHour.Text = "0";
+            // 
+            // lbMinute
+            // 
+            this.lbMinute.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lbMinute.AutoSize = true;
+            this.lbMinute.ForeColor = System.Drawing.Color.RoyalBlue;
+            this.lbMinute.Location = new System.Drawing.Point(103, 122);
+            this.lbMinute.Name = "lbMinute";
+            this.lbMinute.Size = new System.Drawing.Size(14, 16);
+            this.lbMinute.TabIndex = 4;
+            this.lbMinute.Text = "0";
+            // 
+            // lbSecond
+            // 
+            this.lbSecond.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lbSecond.AutoSize = true;
+            this.lbSecond.ForeColor = System.Drawing.Color.RoyalBlue;
+            this.lbSecond.Location = new System.Drawing.Point(155, 122);
+            this.lbSecond.Name = "lbSecond";
+            this.lbSecond.Size = new System.Drawing.Size(14, 16);
+            this.lbSecond.TabIndex = 5;
+            this.lbSecond.Text = "0";
+            // 
+            // lbSplit
+            // 
+            this.lbSplit.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lbSplit.AutoSize = true;
+            this.lbSplit.ForeColor = System.Drawing.Color.RoyalBlue;
+            this.lbSplit.Location = new System.Drawing.Point(207, 122);
+            this.lbSplit.Name = "lbSplit";
+            this.lbSplit.Size = new System.Drawing.Size(14, 16);
+            this.lbSplit.TabIndex = 6;
+            this.lbSplit.Text = "0";
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.AutoSize = true;
+            this.label1.ForeColor = System.Drawing.Color.RoyalBlue;
+            this.label1.Location = new System.Drawing.Point(207, 90);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(25, 16);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "SS";
+            // 
+            // label2
+            // 
+            this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label2.AutoSize = true;
+            this.label2.ForeColor = System.Drawing.Color.RoyalBlue;
+            this.label2.Location = new System.Drawing.Point(155, 90);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(16, 16);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "S";
+            // 
+            // label3
+            // 
+            this.label3.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label3.AutoSize = true;
+            this.label3.ForeColor = System.Drawing.Color.RoyalBlue;
+            this.label3.Location = new System.Drawing.Point(103, 90);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(18, 16);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "M";
+            // 
+            // label4
+            // 
+            this.label4.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label4.AutoSize = true;
+            this.label4.ForeColor = System.Drawing.Color.RoyalBlue;
+            this.label4.Location = new System.Drawing.Point(51, 90);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(17, 16);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "H";
+            // 
+            // btnRes
+            // 
+            this.btnRes.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnRes.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnRes.ForeColor = System.Drawing.Color.RoyalBlue;
+            this.btnRes.Location = new System.Drawing.Point(144, 159);
+            this.btnRes.Name = "btnRes";
+            this.btnRes.Size = new System.Drawing.Size(111, 34);
+            this.btnRes.TabIndex = 12;
+            this.btnRes.Text = "RESET";
+            this.btnRes.UseVisualStyleBackColor = true;
+            this.btnRes.Click += new System.EventHandler(this.btnRes_Click);
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnAdd.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnAdd.ForeColor = System.Drawing.Color.RoyalBlue;
+            this.btnAdd.Location = new System.Drawing.Point(27, 159);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(111, 34);
+            this.btnAdd.TabIndex = 11;
+            this.btnAdd.Text = "ADD";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // lsTimes
+            // 
+            this.lsTimes.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lsTimes.ForeColor = System.Drawing.Color.RoyalBlue;
+            this.lsTimes.FormattingEnabled = true;
+            this.lsTimes.ItemHeight = 16;
+            this.lsTimes.Location = new System.Drawing.Point(27, 208);
+            this.lsTimes.Name = "lsTimes";
+            this.lsTimes.Size = new System.Drawing.Size(228, 132);
+            this.lsTimes.TabIndex = 13;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(282, 353);
+            this.Controls.Add(this.lsTimes);
+            this.Controls.Add(this.btnRes);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.lbSplit);
+            this.Controls.Add(this.lbSecond);
+            this.Controls.Add(this.lbMinute);
+            this.Controls.Add(this.lbHour);
+            this.Controls.Add(this.txtReset);
+            this.Controls.Add(this.txtState);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.Text = "Timer";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button txtState;
+        private System.Windows.Forms.Button txtReset;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label lbHour;
+        private System.Windows.Forms.Label lbMinute;
+        private System.Windows.Forms.Label lbSecond;
+        private System.Windows.Forms.Label lbSplit;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button btnRes;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.ListBox lsTimes;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Timer
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        Stopwatch stopwatch = new Stopwatch();
+        string StateS = "START";
+        int hour = 0, minute = 0, second = 0;
+        double splits = 0;
+
+        private void txtState_Click(object sender, EventArgs e)
+        {
+            stopwatch.Start();
+            if (StateS == "START")
+            {
+                txtState.Text = "STOP";
+                StateS = "STOP";
+                timer1.Start();
+            }
+            else if (StateS == "STOP")
+            {
+                txtState.Text = "START";
+                StateS = "START";
+                timer1.Stop();
+            }
+        }
+
+        int rcr = 0;
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            rcr++;
+            lsTimes.Items.Add(rcr + ": " + lbHour.Text + ":" + lbMinute.Text + ":" + lbSecond.Text + "." + lbSplit.Text);
+            lsTimes.SelectedIndex = lsTimes.Items.Count - 1;
+        }
+
+        private void btnRes_Click(object sender, EventArgs e)
+        {
+            lsTimes.Items.Clear();
+            rcr = 0;
+        }
+
+        private void txtReset_Click(object sender, EventArgs e)
+        {
+            timer1.Stop();
+            hour = 0;
+            minute = 0;
+            second = 0;
+            txtState.Text = "START";
+            StateS = "START";
+
+            lbHour.Text = "0";
+            lbMinute.Text = "0";
+            lbSecond.Text = "0";
+            lbSplit.Text = "0";
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            splits += 1;
+            lbSplit.Text = Convert.ToString(Convert.ToInt16(splits));
+            if (splits>=9)
+            {
+                splits = 0;
+                second++;
+                lbSecond.Text = Convert.ToString(second);
+            }
+            if (second == 60)
+            {
+                second = -1;
+                minute++;
+            }
+            else if (minute == 60)
+            {
+                minute = 0;
+                second = 0;
+                hour++;
+            }
+            lbHour.Text = Convert.ToString(hour);
+            lbMinute.Text = Convert.ToString(minute);
+            lbSecond.Text = Convert.ToString(second);
+            lbSplit.Text = Convert.ToString(Convert.ToInt16(splits));
+        }
+    }
+}
